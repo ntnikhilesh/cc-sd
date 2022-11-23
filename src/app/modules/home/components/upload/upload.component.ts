6d80414d477e5d7f482b1e10701f1916e21668ff
nikhilrzp <85875053+nikhilrzp@users.noreply.github.com>
2022-11-23 16:36:21 +0530

    progress bar

--- src/app/modules/home/components/upload/upload.component.ts
+++ src/app/modules/home/components/upload/upload.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HomeService } from '../../home.service';
-import { LoaderService } from '../../../loader/loader.service'
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { HomeService } from "../../home.service";
+import { LoaderService } from "../../../loader/loader.service";
 declare var $: any;
 @Component({
-  selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.scss'],
+  selector: "app-upload",
+  templateUrl: "./upload.component.html",
+  styleUrls: ["./upload.component.scss"],
 })
 export class UploadComponent implements OnInit {
   uploadDetails: any = {};
-  constructor(private homeService: HomeService, private router: Router, private loaderService: LoaderService) {}
+  constructor(
+    private homeService: HomeService,
+    private router: Router,
+    private loaderService: LoaderService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -21,7 +25,7 @@ export class UploadComponent implements OnInit {
     this.uploadDetails.selectedFiles = event.target.files;
     for (let i = 0; i < this.uploadDetails.selectedFiles.length; i++) {
       this.uploadDetails.fileNames.push({
-        file_name: this.uploadDetails.selectedFiles[i].name.replace(/\s/g, '_'),
+        file_name: this.uploadDetails.selectedFiles[i].name.replace(/\s/g, "_"),
       });
     }
     this.uploadDetails.fileUrls = [];
@@ -37,8 +41,7 @@ export class UploadComponent implements OnInit {
   }
 
   handleUpload() {
-    $('.modal-backdrop').remove();
-    $('#bulkUploadModal').modal('toggle');
+    this.uploadDetails["progress"] = 1;
     const promiseList = [];
     let payload = {
       file_names: [],
@@ -49,36 +52,42 @@ export class UploadComponent implements OnInit {
     this.loaderService.show();
     this.homeService.getPresignedUrls(payload).subscribe(
       (getPresignedUrlsResp) => {
-        this.uploadDetails['getPresignedUrlsResp'] = getPresignedUrlsResp;
+        this.uploadDetails["progress"] = 25;
+        this.uploadDetails["getPresignedUrlsResp"] = getPresignedUrlsResp;
         for (
           let i = 0;
-          i < this.uploadDetails['getPresignedUrlsResp'].length;
+          i < this.uploadDetails["getPresignedUrlsResp"].length;
           i++
         ) {
           let url =
-            this.uploadDetails['getPresignedUrlsResp'][i]['pre_signed_url'];
+            this.uploadDetails["getPresignedUrlsResp"][i]["pre_signed_url"];
           let imgData = this.uploadDetails.selectedFiles[i];
           promiseList.push(this.homeService.uploadData(url, imgData));
         }
         return Promise.all(promiseList)
           .then((response) => {
-            console.log('uploadData resp::', response);
-            this.uploadDetails['uploadDataResp'] = response;
+            this.uploadDetails["progress"] = 75;
+            console.log("uploadData resp::", response);
+            this.uploadDetails["uploadDataResp"] = response;
             this.uploadAgreements();
           })
           .catch(() => {
             this.loaderService.hide();
-            alert('Error while processing your request. Please try later.');
-            console.log('Error while uploadData...');
+            this.uploadDetails["progress"] = 0;
+
+            alert("Error while processing your request. Please try later.");
+            console.log("Error while uploadData...");
           });
       },
       (error) => {
         this.loaderService.hide();
+        this.uploadDetails["progress"] = 0;
+
         alert(
           error?.error?.message ||
-            'Error while processing your request. Please try later.'
+            "Error while processing your request. Please try later."
         );
-        console.log('getPresignedUrls error...', error);
+        console.log("getPresignedUrls error...", error);
       }
     );
   }
@@ -89,19 +98,28 @@ export class UploadComponent implements OnInit {
       let payload = this.getFinalAgreementPayload();
       this.homeService.uploadAgreements(payload).subscribe(
         (uploadAgreementsResp) => {
+          this.uploadDetails["progress"] = 100;
           console.log("uploadAgreementsResp:", this.uploadDetails);
-          this.uploadDetails['uploadAgreementsResp'] = uploadAgreementsResp;
-          alert('Upload Successful!')
-          this.router.navigate([`upload-history-details/${uploadAgreementsResp['history'][0]['_id']}`])
+          this.uploadDetails["uploadAgreementsResp"] = uploadAgreementsResp;
+          alert("Upload Successful!");
+          this.router.navigate([
+            `upload-history-details/${uploadAgreementsResp["history"][0]["_id"]}`,
+          ]);
           this.loaderService.hide();
+          $(".modal-backdrop").remove();
+          $("#bulkUploadModal").modal("toggle");
+          setTimeout(() => {
+            this.uploadDetails["progress"] = 0;
+          }, 1000);
           resolve(true);
         },
         (error) => {
           this.loaderService.hide();
-          console.log('uploadAgreementsResp error...', error);
+          this.uploadDetails["progress"] = 0;
+          console.log("uploadAgreementsResp error...", error);
           alert(
             error?.error?.message ||
-              'Error while processing your request. Please try later.'
+              "Error while processing your request. Please try later."
           );
           reject();
         }
@@ -113,21 +131,21 @@ export class UploadComponent implements OnInit {
   getFinalAgreementPayload() {
     let payload = {
       pdf_file_urls: [],
-      csv_file_url: '',
+      csv_file_url: "",
     };
     this.uploadDetails.getPresignedUrlsResp.forEach((element) => {
-      if (element.content_type === 'application/pdf') {
+      if (element.content_type === "application/pdf") {
         payload.pdf_file_urls.push(element.get_url);
       }
-      if (element.content_type === 'text/csv') {
+      if (element.content_type === "text/csv") {
         payload.csv_file_url = element.get_url;
       }
     });
     return payload;
   }
 
-  onRemoveClick(index){
-    console.log(index, 'onRemoveClick:',this.uploadDetails )
+  onRemoveClick(index) {
+    console.log(index, "onRemoveClick:", this.uploadDetails);
     this.uploadDetails.fileNames.splice(index, 1);
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,11 +8,12 @@ import { LoaderModule } from '../modules/loader/loader.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatTableModule} from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar'
 
 @NgModule({
   declarations: [HistoryComponent, NavComponent],
-  imports: [CommonModule, SharedRoutingModule, FormsModule, ReactiveFormsModule, LoaderModule, MatTableModule, MatPaginatorModule],
-  exports: [HistoryComponent, NavComponent, FormsModule, ReactiveFormsModule, LoaderModule, MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, SharedRoutingModule, FormsModule, ReactiveFormsModule, LoaderModule, MatTableModule, MatPaginatorModule, MatProgressBarModule],
+  exports: [HistoryComponent, NavComponent, FormsModule, ReactiveFormsModule, LoaderModule, MatTableModule, MatPaginatorModule, MatProgressBarModule],
   entryComponents: [],
 })
 export class SharedModule {}
